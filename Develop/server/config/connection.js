c475110e608fb504900131934ba55bcd8c65eea9
PeterJanasz <petertjanasz@gmail.com>
2023-11-29 14:38:12 -0700

    restructured folder and file lay out.

--- Develop/server/config/connection.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bookFinder');
-module.exports = mongoose.connection;
---
+++ server/config/connection.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bookFinder');
+module.exports = mongoose.connection;
